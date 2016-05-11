bf22fc349f23b40b2ef15521d4fbea6fc008277b
deprilula28 <deprilula28@gmail.com>
2016-05-11 20:00:52 -0300

    Fix textNode naming references

---
+++ js/website.js
@@ -0,0 +1,16 @@
+function load(authentication){
+	
+	getScript("youtubeAPIv3", "youtubeAPIv3", function(){
+		getHeader();
+	});
+	
+}
+
+function getHeader(){
+	
+	var navbarFixed = newDiv("navbar-fixed");
+	var nav = document.createElement("nav");
+	
+	var navWrapper = newDiv("nav-wrapper white");
+	
+}
