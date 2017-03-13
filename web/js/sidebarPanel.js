91771368c9e118d141b905d10335fc5419fc7319
deprilula28 <deprilula28@gmail.com>
2017-03-13 17:58:31 -0300

    Fixed sidebar panel, added like & dislike bindings

--- web/js/sidebarPanel.js
+++ web/js/sidebarPanel.js
@@ -2,6 +2,7 @@ UIManager.prototype.loadSidebarPanelNoAuth = function(){
 
   console.log("Asked to load sidebar panel without authentication.");
   var uiMan = youtubeSexy.ui;
+  this.uiMan = uiMan;
   var sidebar = $("#sidebar").get(0);
   this.sidebar = sidebar;
 
@@ -13,8 +14,8 @@ UIManager.prototype.loadSidebarPanelNoAuth = function(){
 
 UIManager.prototype.addButton = function(name, handleClicked){
 
-  var li = uiMan.generateNewElement("li", ["bold"], undefined, this.sidebar, undefined);
-  var btn = uiMan.generateNewElement("a", ["waves-effect", "waves-dark"], name, li, undefined);
+  var li = this.uiMan.generateNewElement("li", ["bold"], undefined, this.sidebar, undefined);
+  var btn = this.uiMan.generateNewElement("a", ["waves-effect", "waves-dark"], name, li, undefined);
   btn.onclick = handleClicked;
 
 }
