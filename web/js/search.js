61be7e634163ac1919ea0c7ae19432cfbfaf9e41
deprilula28 <deprilula28@gmail.com>
2017-04-06 21:09:49 -0300

    Work on the search bar

--- web/js/search.js
+++ web/js/search.js
@@ -14,18 +14,18 @@ UIManager.prototype.loadSearchButton = function(){
             "left": (searchButtonRect.left + e.pageX) + "px",
             "top": (-150 + (e.pageY - $(document.body).scrollTop() - searchButtonRect.top)) + "px"
         });
-                         
-        $(".search-icon-hide").css({"opacity": 1}).animate({"opacity": 0}, 300, "linear", () => {
-            $(".search-icon-hide").css({"opacity": "", "display": "none"});
-            $(".search-icon-show").css({"opacity": 0, "display": ""}).animate({"opacity": 1}, 300, "linear", () => {
-                $(".search-icon-show").css({"opacity": ""});
-                $("#searchInput").focus();
-            });
-        });
+        $(".search-icon-show-row").css({"display": "block"});
+        $(".searchEffectRipple").get(0).appendChild($(".search-icon-show").get(0));
         
         setTimeout(function() {
+            $(".search-icon-show-wrapper").get(0).appendChild($(".search-icon-show").get(0));
             $(wave).remove();
             $("nav").css({"background-color": "#D6D6D6"}); 
+                $("#searchInput").focus();
+
+                $("#searchInput").focusout(() => {
+                    youtubeSexy.ui.unloadSearchBar();
+                });
         }, 300);
     });
 
@@ -37,6 +37,7 @@ UIManager.prototype.unloadSearchBar = function(){
         this.searchBar = false;
         $(".search-icon-hide").css({"opacity": 0, "display": ""}).animate({"opacity": 1}, 300, "linear", () => {
             $(".search-icon-hide").css({"opacity": ""});
+            $(".search-icon-show-row").css({"display": ""});
             $(".search-icon-show").css({"opacity": 1}).animate({"opacity": 0}, 300, "linear", () => {
                 $(".search-icon-show").css({"opacity": "", "display": "none"});
             });
